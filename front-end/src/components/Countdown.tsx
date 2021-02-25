1e401805f35b8eb0f3138bb8122610773ff943eb
Diogo <diogomartins200214@gmail.com>
2021-02-25 09:27:24 +0000

    ChallengeBox component with not active styling

--- front-end/src/components/Countdown.tsx
+++ front-end/src/components/Countdown.tsx
@@ -55,6 +55,7 @@ export function Countdown() {
           className={styles.countdownButton}
         >
          Ciclo encerrado
+         <img src="/icons/check_circle.svg" alt="Ciclo encerrado"/>
         </button>
       ) : (
         <>
@@ -65,6 +66,7 @@ export function Countdown() {
             onClick={resetCountDown}
           >
             Abandonar ciclo
+            <img src="/icons/close.svg" alt="Ciclo encerrado"/>
           </button>
         ) : (
           <button 
@@ -73,6 +75,7 @@ export function Countdown() {
             onClick={startCountdown}
           >
             Iniciar um ciclo
+            <img src="/icons/play-arrow.svg" alt="Ciclo encerrado"/>
           </button>
          )}
         </>
