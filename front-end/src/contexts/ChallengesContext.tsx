bd58e7d637e4846d4ada81de1672c76f8feb312b
Diogo <diogomartins200214@gmail.com>
2021-02-25 10:36:39 +0000

    Business rules

--- front-end/src/contexts/ChallengesContext.tsx
+++ front-end/src/contexts/ChallengesContext.tsx
@@ -1,29 +1,51 @@
 import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
 
-type ChallengesContexData = {
+type Challenge = {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+type ChallengesContextData = {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  experienceToNextLevel: number;
+  activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 type ChallengesProviderProps = {
   children: ReactNode
 }
 
-export const ChallengesContext = createContext({} as ChallengesContexData);
+export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
-  const [level, setLeve] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(30);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
   const levelUp = () => {
-    setLeve(level + 1);
+    setLevel(level + 1);
   }
 
   const startNewChallenge = () => {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  const resetChallenge = () => {
+    setActiveChallenge(null);
   }
 
   return (
@@ -32,8 +54,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
       level, 
       currentExperience, 
       challengesCompleted,
+      activeChallenge,
+      experienceToNextLevel,
       levelUp, 
       startNewChallenge,
+      resetChallenge,
       }}
     >
       {children}
