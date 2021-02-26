50491a2e814dbee5349895eeeef0d71a7c29ac59
Diogo Martins <diogo-pop@pop-os.localdomain>
2021-02-26 11:58:15 +0000

    Cookie storage

--- front-end/src/contexts/ChallengesContext.tsx
+++ front-end/src/contexts/ChallengesContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
 
 type Challenge = {
@@ -20,15 +21,18 @@ type ChallengesContextData = {
 }
 
 type ChallengesProviderProps = {
-  children: ReactNode
+  children: ReactNode;
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
-export function ChallengesProvider({ children }: ChallengesProviderProps) {
-  const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
-  const [challengesCompleted, setChallengesCompleted] = useState(0);
+export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(rest.level ?? 1);
+  const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
+  const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
 
@@ -36,7 +40,13 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   useEffect(() => {
     Notification.requestPermission();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    Cookies.set('level', String(level));
+    Cookies.set('currentExperience', String(currentExperience));
+    Cookies.set('challengesCompleted', String(challengesCompleted));
+  },[level, currentExperience, challengesCompleted]);
 
   const levelUp = () => {
     setLevel(level + 1);
--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -5,9 +5,7 @@ import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
 
