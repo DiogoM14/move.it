a4d99acba8cee3998b6926754f9739522607dfef
DiogoM14 <diogomartins200214@gmail.com>
2021-02-26 20:06:07 +0000

    Profile Component

--- moveit-2.0/src/pages/home.tsx
+++ moveit-2.0/src/pages/home.tsx
@@ -1,17 +1,18 @@
 import styles from '../styles/pages/Home.module.css';
+import { Profile } from './components/Profile';
 import { SideBar } from './components/SideBar';
 
 export default function Home() {
   return (  
     <div className={styles.wrapper}>
       <SideBar />
-
+      
       <div className={styles.homeContainer}>
-        <header>
-          <h1>XP</h1>
-        </header>
         <section>
-
+          <div>
+            <Profile />
+          </div>
+          <div></div>
         </section>
       </div>
     </div>
