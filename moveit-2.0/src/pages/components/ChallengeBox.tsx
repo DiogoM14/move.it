29eee59a7004ebd9aad802e892ede028b03e5702
DiogoM14 <diogomartins200214@gmail.com>
2021-02-26 21:25:07 +0000

    Fixing css Bugs

---
+++ moveit-2.0/src/pages/components/ChallengeBox.tsx
@@ -0,0 +1,9 @@
+import styles from '../../styles/components/ChallengeBox.module.css';
+
+export function ChallengeBox() {
+  return (
+    <div className={styles.challengeBoxContainer}>
+      <h1>Teste</h1>
+    </div>
+  )
+}
--- moveit-2.0/src/pages/home.tsx
+++ moveit-2.0/src/pages/home.tsx
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
+import { ChallengeBox } from './components/ChallengeBox';
 import { CompletedChallenges } from './components/CompletedChallenges';
 import { Profile } from './components/Profile';
 import { SideBar } from './components/SideBar';
@@ -6,6 +8,11 @@ import { SideBar } from './components/SideBar';
 export default function Home() {
   return (  
     <div className={styles.wrapper}>
+
+      <Head>
+        <title>Home | Move.it</title>
+      </Head>
+
       <SideBar />
       
       <div className={styles.homeContainer}>
@@ -14,7 +21,9 @@ export default function Home() {
             <Profile />
             <CompletedChallenges />
           </div>
-          <div></div>
+          <div>
+            <ChallengeBox />
+          </div>
         </section>
       </div>
     </div>
