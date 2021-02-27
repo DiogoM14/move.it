aacf1c8f32ce7372d263f6fc60343e2f86f1b659
DiogoM14 <diogomartins200214@gmail.com>
2021-02-27 15:51:04 +0000

    Countdown Button

--- moveit-2.0/src/pages/components/CountDown.tsx
+++ moveit-2.0/src/pages/components/CountDown.tsx
@@ -2,16 +2,23 @@ import styles from '../../styles/components/CountDown.module.css';
 
 export function CountDown() {
   return (
-    <div className={styles.countdownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>2</span>
+          <span>5</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>0</span>
+          <span>0</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
-    </div>
-  )
-}
+
+      <button className={styles.countdownButton}>
+        Iniciar um ciclo
+        <img src="/icons/play-arrow.svg" alt="Iniciar ciclo"/>
+      </button>
+    </>
+    )
+  }
